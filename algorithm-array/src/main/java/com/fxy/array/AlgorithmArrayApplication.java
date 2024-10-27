236482d97fc06836c7283cc578e6af10025996fa
fxy <fuxiaoyang@cmiot.cmcc>
2024-10-27 11:08:55 +0800

    添加算法数组模块

---
+++ algorithm-array/src/main/java/com/fxy/array/AlgorithmArrayApplication.java
@@ -0,0 +1,13 @@
+package com.fxy.array;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmArrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmArrayApplication.class, args);
+    }
+
+}
---
+++ algorithm-array/src/test/java/com/fxy/array/AlgorithmArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxy.array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmArrayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
